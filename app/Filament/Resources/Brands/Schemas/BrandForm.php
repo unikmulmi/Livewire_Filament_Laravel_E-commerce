c86d91c1868a3f0baa990b5321a9dc47a04e4040
unik <unik_mulmi7@hotmail.com>
2026-02-18 23:44:20 +0545

    filament Brand Completed

---
+++ app/Filament/Resources/Brands/Schemas/BrandForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\Brands\Schemas;
+
+use App\Models\Brand;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Str;
+
+class BrandForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->debounce(2000)
+                    ->afterStateUpdated(fn($state , $set) => $set('slug' , Str::slug($state))),
+                TextInput::make('slug')
+                    ->maxLength(255)
+                    ->required()
+                    ->disabled()
+                    ->dehydrated()
+                    ->unique(Brand::class , 'slug' , ignoreRecord:true)
+                    ->columnSpanFull(),
+                FileUpload::make('image')
+                    ->image()
+                    ->directory('brands'),
+                Toggle::make('is_active')
+                    ->required(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Brands/Tables/BrandsTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources\Brands\Tables;
+
+use Filament\Actions\ActionGroup;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class BrandsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->searchable(),
+                ImageColumn::make('image'),
+                IconColumn::make('is_active')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ActionGroup::make([
+                    EditAction::make(),
+                    ViewAction::make(),
+                    DeleteAction::make(),
+                ])
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
