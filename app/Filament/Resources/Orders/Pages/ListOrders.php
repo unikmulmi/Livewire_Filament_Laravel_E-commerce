346d2d4a5d71e07f231bf510abb1cd97c7dcbfc8
unik <unik_mulmi7@hotmail.com>
2026-02-19 10:40:57 +0545

    widgets orders

--- app/Filament/Resources/Orders/Pages/ListOrders.php
+++ app/Filament/Resources/Orders/Pages/ListOrders.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Orders\Pages;
 
 use App\Filament\Resources\Orders\OrderResource;
+use App\Filament\Resources\Orders\Widgets\OrderStats;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
 
 class ListOrders extends ListRecords
 {
@@ -14,6 +16,25 @@ class ListOrders extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return[
+            null => Tab::make('All'),
+            'new' => Tab::make()->query(fn ($query) => $query->where('status' , 'new')),
+            'processing' => Tab::make()->query(fn ($query) => $query->where('status' , 'processing')),
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status' , 'shipped')),
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status' , 'delivered')),
+            'cancelled' => Tab::make()->query(fn ($query) => $query->where('status' , 'cancelled')),
         ];
     }
 }
