59af69d794f4e96e0a5603fb01b7ec9f4f54c5a1
unik <unik_mulmi7@hotmail.com>
2026-04-15 16:52:06 +0545

    Payment_Status logic updated for status

--- app/Livewire/SuccessPage.php
+++ app/Livewire/SuccessPage.php
@@ -5,15 +5,35 @@ namespace App\Livewire;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 #[Title('Success - ShopHeX')]
 
 class SuccessPage extends Component
 {
+    #[Url]
+    public $session_id;
+
     public function render()
     {
         $latest_order = Order::with('address')->where('user_id' , Auth::user()->id)->latest()->first();
+
+        if( $this->session_id){
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+            $session_info = Session::retrieve($this->session_id);
+
+            if ( $session_info->payment_status != 'paid'){
+                $latest_order->payment_status = 'failed';
+                $latest_order->save();
+                return redirect()->route('cancel');
+            } elseif ( $session_info->payment_status == 'paid'){
+                $latest_order->payment_status = 'paid';
+                $latest_order->save();
+            }
+        }
 
         return view('livewire.success-page' , [
             'order' => $latest_order,
