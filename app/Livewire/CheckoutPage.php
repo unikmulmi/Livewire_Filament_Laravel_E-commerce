8b2a92c515673319e3896011401845fd798acb1a
unik <unik_mulmi7@hotmail.com>
2026-04-15 14:13:25 +0545

    CheckoutPage - Payment logic .. using STRIPE

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,8 +3,13 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Models\Address;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 #[Title('Checkout Page - ShopHeX')]
 
@@ -19,6 +24,13 @@ class CheckoutPage extends Component
     public $zip_code;
     public $payment_method;
 
+    public function mount(){
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        if(count($cart_items) == 0){
+            return redirect('/products');
+        }
+    }
+
     public function placeOrder()
     {
         $this->validate([
@@ -31,6 +43,94 @@ class CheckoutPage extends Component
             'zip_code' => 'required',
             'payment_method' => 'required',
         ]);
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+
+        $line_items = [];
+
+        foreach ($cart_items as $item) {
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'nrs',
+                    'unit_amount' => $item['unit_amount'],
+                    'product_data' => [
+                        'name' => $item['name'],
+                    ],
+                ],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
+        $order = new Order();
+        $order->user_id = Auth::user()->id;
+        $order->grand_total = CartManagement::calculateGrandTotal($cart_items);
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'nrs';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order Placed By : ' . Auth::user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street_address = $this->street_address;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+
+        $redirect_url = '';
+
+        if( $this->payment_method == 'stripe'){
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+
+            $usd_rate = 0.0075;
+
+            $stripe_line_items = [];
+
+            foreach ($cart_items as $item) {
+
+                $price_npr = $item['unit_amount']; 
+
+                $price_usd = $price_npr * $usd_rate;
+
+                $unit_amount = (int) round($price_usd * 100);
+
+                $stripe_line_items[] = [
+                    'price_data' => [
+                    'currency' => 'usd',
+                    'unit_amount' => $unit_amount,
+                    'product_data' => [
+                        'name' => $item['name'],
+                    ],
+                ],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
+            $sessionCheckout = Session::create([
+                'payment_method_types' => ['card'],
+                'customer_email' => Auth::user()->email,
+                'line_items' => $stripe_line_items,
+                'mode' => 'payment',
+                'success_url' => route('success').'?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => route('cancel'),
+            ]);
+
+            $redirect_url = $sessionCheckout->url;
+        }
+        else{
+            $redirect_url = route('success');
+        }
+
+        $order->save();
+        $address->order_id = $order->id;
+        $address->save();
+        $order->items()->createMany($cart_items);
+        CartManagement::clearCartItemsFromCookie();
+        return redirect($redirect_url);
     }
 
     public function render()
--- resources/views/livewire/checkout-page.blade.php
+++ resources/views/livewire/checkout-page.blade.php
@@ -99,11 +99,11 @@
 						</label>
 					</li>
 					<li>
-						<input wire:model="payment_method" class="hidden peer" id="hosting-big" name="hosting" type="radio" value="paypal" />
+						<input wire:model="payment_method" class="hidden peer" id="hosting-big" name="hosting" type="radio" value="stripe" />
 						<label class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700" for="hosting-big">
 							<div class="block">
 								<div class="w-full text-lg font-semibold">
-									Paypal
+									Stripe
 								</div>
 							</div>
 							<svg aria-hidden="true" class="w-5 h-5 ms-3 rtl:rotate-180" fill="none" viewbox="0 0 14 10" xmlns="http://www.w3.org/2000/svg">
